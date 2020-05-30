ef33cf441afd4b33419056820deceb62a95b7462
nishantasarma <nishantasarma@gmail.com>
2020-05-30 08:43:12 +0530

    added code to display db

--- umapApp/models.py
+++ umapApp/models.py
@@ -3,7 +3,7 @@ from django.db import models
 # Create your models here.
 class UrlMap(models.Model):
 
-    longurl = models.CharField(max_length=30, primary_key=True)
+    longurl = models.CharField(max_length=30, primary_key=True, null=False, blank=False)
     shorturl = models.CharField(max_length=4)
 
 
--- umapApp/views.py
+++ umapApp/views.py
@@ -12,11 +12,18 @@ def home_view(request):
 def get_data(request, *args, **kwargs):
     rand_int = str(random.randint(1000,9999))
     long_url = None
+    query_set = ''
     if request.method == 'GET':
         long_url = request.GET['key']
         p = UrlMap(longurl=long_url, shorturl=rand_int)
         p.save()
-        return HttpResponse('success')
+        results = UrlMap.objects.all()
+        #print(results[0])
+        if results:
+            for result in results:
+                query_set = query_set + str(result) +'\r\n'
+
+        return HttpResponse(query_set)
 
     else:
         return HttpResponse('Fail')
